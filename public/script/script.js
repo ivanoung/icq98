95a8167f02f1d3249fe792ab2b4a0c839c9a0cfe
ivanoung <ivanoung@gmail.com>
2018-05-01 20:14:32 +0800

    Setup jQuery event and setup CSS for the message box side

--- public/script/script.js
+++ public/script/script.js
@@ -5,25 +5,45 @@ function Sound(src) {
   this.sound.setAttribute("controls", "none");
   this.sound.style.display = "none";
   document.body.appendChild(this.sound);
-  this.play = function() {
+  this.play = function () {
     this.sound.play();
   };
-  this.stop = function() {
+  this.stop = function () {
     this.sound.pause();
   };
 }
 
-$(function() {
+$(function () {
   const uhoh = new Sound("./sound-effects/icq-message.wav");
   const socket = io();
 
-  $("#send").on("click", function() {
+  $("#send").on("click", function () {
     socket.emit("chat message", $("#m").val());
     $("#m").val("");
     return false;
   });
-  socket.on("chat message", function(msg) {
+
+  $(document).on('keydown', function (event) {
+    if (event.keyCode === 13) {
+      socket.emit("chat message", $("#m").val());
+      $("#m").val("");
+      return false;
+    }
+  })
+
+
+
+  socket.on("chat message", function (msg) {
     $("#messages").append($("<li>").text(msg));
     uhoh.play();
   });
 });
+
+
+$(() => {
+  $(document).on('keydown', function (event) {
+    if (event.keyCode === 13) {
+
+    }
+  })
+})
